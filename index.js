63709496bda4bb8cfa4c3cf31f1bc22ecd1b4641
AlexC0903 <44229612+AlexC0903@users.noreply.github.com>
2020-02-11 16:38:08 +0700

    static.html
    static.html file added

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hi")
+const express = require('express')
+const app = express()
+express.static('public')
+const port = 3000
+
+
+app.use('/spider', express.static('public'))
+app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/hello_bob', (req, res) => res.send('Hello Bob!'))
+app.get('/hello_2', (req, res) => res.send('Test'))
+app.get('/hello*', (req, res) => res.send('Hello anything!'))
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
